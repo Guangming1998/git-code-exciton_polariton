50023e575dd2e7c68e7753fe541abd5544f63082
Guangming Liu <gliu8@nd.edu>
2024-04-14 16:49:39 -0400

    line 129-130: check if initial state function written correctly. line 137-138: 'switch' for diagnal disorder. line 140-142: sanitycheck. line 141, line 146-154: current correlation for static Hamiltonian by exp(-i*H*t) factor, no numerical simulation. line 142, line 156-172: current correlation for numerical simulation (useful for time dependent hamiltonian) line 173-183: SanityCheck for two different method, which gave same result in the end.

--- withCavity/test_cavity.py
+++ withCavity/test_cavity.py
@@ -2,8 +2,8 @@ import sys
 import numpy as np
 import pandas as pd
 import os # try to use 'os' module to create new file directory for output files.
-from trajectory import Trajectory_SSHmodel
-path = sys.argv[-1].replace('displacement.py','')
+from Cavity_ssh import Trajectory_SSHmodel
+path = sys.argv[-1].replace('test_cavity.py','')
 'check my path'
 # print(path)
 
@@ -20,6 +20,8 @@ except :
     print("Error occure while creating the directories.")
 plotResult = False
 printOutput = False
+SanityCheck = True
+
 if '--print' in sys.argv:
     printOutput = True
 if '--plot' in sys.argv: 
@@ -65,22 +67,22 @@ else:
     # wavenumber_to_au = 4.55634e-6 # energy from wavenumber to atomic unit
     # Adot_to_au = 1.88973 #angstrom to atomic length
     # dt = 1#0.025e-3*ps_to_au
-    Ntimes = 400000
-    Nskip = 10
+    Ntimes = 2000
+    Nskip = 1
     
     # hbar_to_amuAps = 1.1577e4
     
     #conversion 
-    Nmol = 5
+    Nmol = 20
     
     if atomic_unit:
-        staticCoup = 2 #300 *wavenumber_to_au
+        staticCoup = 0.5#300 *wavenumber_to_au
         dynamicCoup = 0*995/Adot_to_au * wavenumber_to_au
-        kBT = 104.3*wavenumber_to_au
+        kBT = 0.1#104.3*wavenumber_to_au
         mass = 250*amu_to_auMass
         Kconst = 14500*amu_to_auMass/(ps_to_au**2)
         hbar = 1
-        dt = 0.025e-3*ps_to_au
+        dt = 0.01#0.025e-3*ps_to_au
         couplingstrength = 2#50 *wavenumber_to_au
         cavityFrequency = 1#1500*wavenumber_to_au
     else: #"use amu*A^2*ps^-2 unit"
@@ -92,7 +94,7 @@ else:
         hbar = 1*hbar_to_amuAps
         dt = 0.025e-3
     
-    
+    useDiagonalDisorder=True
 
     # Nmol = 101
     # Wmol =  0.0
@@ -124,12 +126,63 @@ model1 = Trajectory_SSHmodel(Nmol,hbar)
 # print(model1.Nmol)
 model1.initialGaussian(kBT,mass,Kconst)
 model1.initialHamiltonian(staticCoup,dynamicCoup)
+# model1.initialCj_disorder_trajectory(kBT)
+# Prob = model1.Prob
 
-model1.initialHamiltonianCavity(couplingstrength,cavityFrequency)
-model1.initialState(hbar,kBT,most_prob=False)
+# model1.initialHamiltonianCavity(couplingstrength,cavityFrequency)
+# model1.initialState(hbar,kBT,most_prob=False)
 # model1.initialstate_equal()
 # print(model1.Htot)
 # eigen_energy = model1.eigenvalue()
+if useDiagonalDisorder:
+    model1.updateDiagonalStaticDisorder(1e-9*staticCoup)
+    
+if useDiagonalDisorder or SanityCheck:
+    model1.initialCj_disorder()
+    model1.initialCj_disorder_trajectory(kBT)
+
+    # model1.initial_densityMatrix(kBT)
+    
+    CJJavg1 = model1.getCurrentCorrelation_static(dt,Ntimes,kBT,0.0)
+    times = []
+    CJJavg2_list = np.zeros((Ntimes,Nmol),complex)
+    CJJavg1_list = []
+    Prob = model1.Prob
+    J0Cj = np.zeros((Nmol,Nmol),complex)
+    for i in range(Ntimes):
+        times.append(dt*i)
+        CJJavg1_list.append(CJJavg1[i])
+    
+    for j in range(Nmol):
+        J0Cj[:,j] = np.dot(model1.Jt0,model1.Cj[:,j])
+    CJJavg1_list = np.array(CJJavg1_list,complex)
+    
+    # CJJavg1_list = np.array 
+    for i in range(Nmol):
+        for it in range(Ntimes):
+            # J0Cj = np.dot(model1.Jt0,model1.Cj[i])
+            if it ==0:
+                CJJavg2_list[it,i] = model1.getCurrentCorrelation_disorder_trajectory(model1.Cj[:,i],J0Cj[:,i])
+            model1.Cj[:,i] = model1.RK4_Cj_trajectory(dt,model1.Cj[:,i])
+            J0Cj[:,i] = model1.propagateJ0Cj_RK4_trajectory(dt,J0Cj[:,i])
+            if it%Nskip==0 and it!=0 :
+                CJJavg2_list[it,i] = model1.getCurrentCorrelation_disorder_trajectory(model1.Cj[:,i],J0Cj[:,i])
+                
+            
+    CJJavg2_list = np.dot(CJJavg2_list,Prob)
+    if SanityCheck:
+        for it in range(len(times)):
+            print(it,CJJavg1_list[it], CJJavg2_list[it], np.abs(CJJavg1_list[it]-CJJavg2_list[it]))
+            
+    
+        fig,ax = plt.subplots()
+        ax.plot(times,np.real(CJJavg1_list),'-r+')
+        ax.plot(times,np.real(CJJavg2_list),'-bx')
+        ax.set_xlabel('time')
+        ax.set_ylabel('CJJ')
+        plt.show()
+
+
 'check if the derivation of current operator is correct or not. Result shows that derivation is correct'
 # model1.polarization_operator()
 # J = model1.Current_operator()
@@ -148,10 +201,12 @@ model1.initialState(hbar,kBT,most_prob=False)
 
 'check commutator'
 # print(model1.Hint)
-print(np.dot(model1.Jmol_0,model1.Hint)-np.dot(model1.Hint,model1.Jmol_0))
+# print(np.dot(model1.Jmol_0,model1.Hint)-np.dot(model1.Hint,model1.Jmol_0))
 
 # print(np.dot(model1.Jtot_0,model1.Htot)-np.dot(model1.Htot,model1.Jtot_0))
-print(model1.Htot)
+# print(model1.Htot)
 # print(np.dot(model1.Jcav_0,model1.Hint)-np.dot(model1.Hint,model1.Jcav_0))
 # print(np.dot(model1.Jcav_0,model1.Hint)-np.dot(model1.Hint,model1.Jcav_0))
+# print(np.dot(model1.Jt0,model1.H_disorder)-np.dot(model1.H_disorder,model1.Jt0))
+
 
